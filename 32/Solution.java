b1df87c38c36cf019ed643c0954728db0e2b0181
ken <389079588@qq.com>
2018-02-24 15:22:30 +0800

    Create Solution.java

---
+++ 32/Solution.java
@@ -0,0 +1,17 @@
+//从上到下打印二叉树
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> result=new ArrayList<>();
+        if(root==null)return result;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        //队列不为空
+        while(!queue.isEmpty()){
+            TreeNode temp=queue.poll();
+            result.add(temp.val);
+            if(temp.left!=null)queue.offer(temp.left);
+            if(temp.right!=null)queue.offer(temp.right);
+        }
+        return result;
+    }
+}
