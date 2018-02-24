10fdc9b0e0584494da51d99fc7c8999e232dc6e1
ken <389079588@qq.com>
2018-02-24 15:13:21 +0800

    Create Solution.java

---
+++ 31/Solution.java
@@ -0,0 +1,19 @@
+//栈的压入、弹出序列
+public class Solution {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        Stack<Integer> s = new Stack<Integer>();
+        //用于标识弹出序列的位置
+        int popIndex = 0;
+        for(int i = 0; i< pushA.length;i++){
+            s.push(pushA[i]);
+            //如果栈不为空，且栈顶元素等于弹出序列
+            while(!s.empty() &&s.peek() == popA[popIndex]){
+                //出栈
+                s.pop();
+                //弹出序列向后一位
+                popIndex++;
+            }
+        }
+        return s.empty();
+    }
+}
