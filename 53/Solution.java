68e6f86943529c149558d258f04e3580cdf8f707
ken <389079588@qq.com>
2018-02-26 16:26:56 +0800

    Create Solution.java

---
+++ 53/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int GetNumberOfK(int [] array , int k) {
+       if(array==null||array.length==0)return 0;
+        int index=-1;
+        int start=0;
+        int end=array.length-1;
+        //二分法
+        while(start<=end){
+            int mid=(start+end)>>1;
+            if(array[mid]==k){
+                index=mid;
+                break;
+            }else if(array[mid]>k)
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        //index=-1表明不存在k
+        if(index==-1)return 0;
+        start=index-1;
+        end=index+1;
+        //向前遍历
+        while(start>=0&&array[start]==k)
+            --start;
+        //向后遍历
+        while(end<array.length&&array[end]==k)
+            ++end;
+        return end-start-1;
+    }
+}
